97d0fea19e7a20aabab36a1f061f7e3ca5350af7
randy8642 <randy8642@outlook.com>
2021-11-28 23:17:22 +0800

    20211128

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 from scipy.signal import convolve2d
+import queue
 
 def change2ConvView(x, y):
     # pad
@@ -22,26 +23,6 @@ def createSubsampleImgs(originImage, originTemplate):
     for _ in range(3):
         sample_imgs.append(sample_imgs[-1][::2, ::2])
         sample_templates.append(sample_templates[-1][::2, ::2])
-
-    # openCV CannyEdge
-    # sample_imgs = [cv2.Canny(i, 200, 250) for i in sample_imgs]
-    # sample_templates = [cv2.Canny(i, 200, 250) for i in sample_templates]
-
-    # 
-    # Kx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
-    # Ky = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], np.float32)
-    
-    # def cal_sobel_amp(i):
-    #     G = np.hypot(convolve2d(i, Kx), convolve2d(i, Ky))        
-    #     G = (G - G.min()) / (G.max() - G.min()) * 255
-    #     G[G < 50] = 0
-    #     G[G >= 50] = 255
-
-    #     return G
-    
-
-    # sample_imgs = [cal_sobel_amp(i) for i in sample_imgs]
-    # sample_templates = [cal_sobel_amp(i) for i in sample_templates]
     
     return sample_imgs, sample_templates
 
@@ -50,3 +31,102 @@ def cannyEdgeDetect(img, threshold):
 
 
     return 0
+
+def deduplicate(location: np.ndarray, similarity: np.ndarray, threshold):
+    results_location = []
+    results_sim = []
+
+    while len(location) > 0:    
+        p = location[0]        
+
+        distance = np.sqrt(np.sum((location - p)**2, axis=1))
+        nearPoint = np.argwhere(distance < threshold).flatten()
+
+        sim_argmax = np.argmax(similarity[nearPoint])
+        results_location.append(location[nearPoint][sim_argmax])
+        results_sim.append(similarity[nearPoint][sim_argmax])
+
+        location = np.delete(location, nearPoint, axis=0)
+        similarity = np.delete(similarity, nearPoint, axis=0) 
+    
+    return np.array(results_location), np.array(results_sim)
+
+def templateMatching(img, temp):
+    sub_matrices = change2ConvView(img, temp)
+
+    #
+    h, w = temp.shape
+    T_norm = temp - np.mean(temp)  
+    I_norm_mean = np.einsum('klij->kl', sub_matrices) / (w*h)
+
+    #
+    m = np.einsum('ij,klij->kl', T_norm, sub_matrices)
+    n = I_norm_mean * np.sum(T_norm)
+
+    
+    T_norm_square_sum = np.sum(T_norm**2)    
+    I_norm_square_sum = np.einsum('klij,klij->kl', sub_matrices, sub_matrices) \
+                    - 2 * np.einsum('klij->kl', sub_matrices) * I_norm_mean \
+                    + (w*h) * I_norm_mean**2
+
+    R = (m - n) / np.sqrt(T_norm_square_sum * I_norm_square_sum)
+
+    return R
+
+def drawRec(img, loc, sim, t_w, t_h):
+    for pt, similarity in zip(loc, sim):
+        cv2.rectangle(img, (pt[1] - t_w//2, pt[0] - t_h//2), (pt[1] + t_w//2, pt[0] + t_h//2), (0, 0, 255), 2)
+    for pt, similarity in zip(loc, sim):
+        cv2.putText(img, f'center [{pt[1]},{pt[0]}]', (pt[1] - 50, pt[0] - 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1, cv2.LINE_AA)
+        cv2.putText(img, f'score {round(similarity, 3)}', (pt[1] - 50, pt[0] - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1, cv2.LINE_AA)
+
+    return img
+
+def convertBGR2GRAY(img):
+    res = img[:, :, 2]*0.299 + img[:, :, 1]*0.587 + img[:, :, 0]*0.114
+    res = res.astype(np.uint8)
+    return res
+
+def speedUp_subsample(img, templ, tm_function, threshold):
+    # sub sampling
+    sample_imgs, sample_templates = createSubsampleImgs(img, templ)
+
+    # init. task
+    init_level = len(sample_imgs) - 1
+    que = queue.Queue()
+    # [init_level], [start_point], [w], [h]
+    que.put([init_level, np.array([0, 0]), sample_imgs[-1].shape[1], sample_imgs[-1].shape[0]])
+
+    results_loc = []
+    results_sim = []
+    while not que.empty():
+        now_level, start_point, subimage_w, subImage_h = que.get()  
+
+        now_template = sample_templates[now_level]
+        now_img = sample_imgs[now_level][start_point[0]:start_point[0] + subImage_h, start_point[1]:start_point[1] + subimage_w]
+
+        R = tm_function(now_img, now_template)
+
+        #     
+        loc = np.where( R >= threshold)
+
+        points_loc = np.array([[i, j] for i, j in zip(*loc)])
+        points_sim = np.array([R[i, j] for i, j in zip(*loc)])
+        targetPoint, _ = deduplicate(points_loc, points_sim, sample_imgs[now_level].shape[1]*0.1)
+        
+        if len(targetPoint) <= 0:       
+            continue    
+        #
+        if now_level == 1:  
+            results_loc.append((targetPoint[0] + start_point)*2)      
+            results_sim.append(R[(targetPoint[0])[0], (targetPoint[0])[1]])  
+            continue    
+        #
+        targetPoint = targetPoint + start_point
+        for x, y in targetPoint:
+            h_upper = x - int(now_template.shape[0]*0.5) if x - now_template.shape[0]*0.5 > 0 else 0
+            w_upper = y - int(now_template.shape[1]*0.5) if y - now_template.shape[1]*0.5 > 0 else 0
+
+            que.put([now_level-1, np.array([h_upper, w_upper])*2, int(now_template.shape[1]*2), int(now_template.shape[0]*2)])
+    
+    return results_loc, results_sim
--- main.py
+++ main.py
@@ -1,128 +1,37 @@
 import cv2
 import numpy as np
-import queue
-from functions import change2ConvView, createSubsampleImgs
+import argparse
+from functions import deduplicate, templateMatching, drawRec, convertBGR2GRAY, speedUp_subsample
 
-imgName = 'Die1.tif'
-templateName = 'Die-Template.tif'
+def main(imgPath: str, templatePath: str, threshold: float):
+    img = cv2.imread(imgPath)
+    template = cv2.imread(templatePath)
 
-img = cv2.imread(f'./source/{imgName}')
-template = cv2.imread(f'./template/{templateName}')
+    img_gray = convertBGR2GRAY(img)
+    template_gray = convertBGR2GRAY(template)
 
-img_gray = img[:, :, 2]*0.299 + img[:, :, 1]*0.587 + img[:, :, 0]*0.114
-img_gray = img_gray.astype(np.uint8)
+    results_loc, results_sim = speedUp_subsample(img_gray, template_gray, templateMatching, threshold)
 
-template_gray = template[:, :, 2]*0.299 + template[:, :, 1]*0.587 + template[:, :, 0]*0.114
-template_gray = template_gray.astype(np.uint8)
-
-# sub sampling
-sample_imgs, sample_templates = createSubsampleImgs(img_gray, template_gray)
-
-# init. task
-init_level = len(sample_imgs) - 1
-
-que = queue.Queue()
-
-# [init_level], [start_point], [w], [h]
-que.put([init_level, np.array([0, 0]), sample_imgs[-1].shape[1], sample_imgs[-1].shape[0]])
-
-results = []
-while True:
-    if que.empty():
-        break    
-    
-    now_level, start_point, subimage_w, subImage_h = que.get()  
-
-    now_template = sample_templates[now_level]
-    now_img = sample_imgs[now_level][start_point[0]:start_point[0] + subImage_h, start_point[1]:start_point[1] + subimage_w]
-
-
-    sub_matrices = change2ConvView(now_img, now_template)
+    # 移除重複目標框
+    res_loc, res_sim = deduplicate(np.array(results_loc), np.array(results_sim), img_gray.shape[1]*0.1)
 
     #
-    template_h, template_w = now_template.shape
-    T_norm = now_template - np.mean(now_template)  
-    I_norm_mean = np.einsum('klij->kl', sub_matrices) / (template_w*template_h)
+    img_result = drawRec(img.copy(), res_loc, res_sim, *template_gray.shape[::-1])
 
-    #
-    m = np.einsum('ij,klij->kl', T_norm, sub_matrices)
-    n = I_norm_mean * np.sum(T_norm)
+    # cv2.imshow('result', img_result)
+    # cv2.waitKey(0)
 
-    
-    T_norm_square_sum = np.sum(T_norm**2)    
-    I_norm_square_sum = np.einsum('klij,klij->kl', sub_matrices, sub_matrices) \
-                    - 2 * np.einsum('klij->kl', sub_matrices) * I_norm_mean \
-                    + (template_w*template_h) * I_norm_mean**2
+    # imgbaseName = imgName.split('.')[0]
+    # cv2.imwrite(f'./result/{imgbaseName}.jpg', img_res)
 
-    R = (m - n) / np.sqrt(T_norm_square_sum * I_norm_square_sum)
+if __name__ == '__main__':   
+    imgPath = './source/100-4.jpg'
+    templatePath = './template/100-Template.jpg'
+    threshold = 0.6
 
-    # 
-    threshold = 0.65
-    loc = np.where( R >= threshold)
-  
-    
-    #
-    targetPoint = []
-    for n, (x, y) in enumerate(zip(*loc)):
-        if n == 0:
-            targetPoint.append([x, y])
-        else:
-            dis = [np.sqrt((x-i)**2 + (y-j)**2) for i, j in targetPoint]
-            dis_sort = np.argsort(dis)
-            sim = [R[i, j] for i, j in targetPoint]
-                        
-            if dis[dis_sort[0]] > sample_imgs[now_level].shape[1]*0.1:
-                targetPoint.append([x, y])
-            else:
-                if R[x, y] > sim[dis_sort[0]]:
-                    del targetPoint[dis_sort[0]]
-                    targetPoint.append([x, y])
-            
+    import time
+    st = time.time()
 
-    
-    if len(targetPoint) <= 0:       
-        continue
+    main(imgPath, templatePath, threshold)
 
-    targetPoint = np.array(targetPoint) + start_point
-    
-    #
-    if now_level == 1:          
-        results.append([(targetPoint[0]*2).tolist(), R[(targetPoint[0]-start_point)[0], (targetPoint[0]-start_point)[1]]])
-    else:
-        for x, y in targetPoint:
-            h_upper = x - int(now_template.shape[0]*0.5) if x - now_template.shape[0]*0.5 > 0 else 0
-            w_upper = y - int(now_template.shape[1]*0.5) if y - now_template.shape[1]*0.5 > 0 else 0
-
-            que.put([now_level-1, np.array([h_upper, w_upper])*2, int(now_template.shape[1]*2), int(now_template.shape[0]*2)])
-
-# 移除重複目標框
-deduplicate_results = []
-for n, ((x, y), sim) in enumerate(results):
-    if n == 0:
-        deduplicate_results.append([[x, y], sim])
-    else:
-        dis = [np.sqrt((x-i)**2 + (y-j)**2) for (i, j), s in deduplicate_results]
-        dis_sort = np.argsort(dis)
-                    
-        if dis[dis_sort[0]] > sample_imgs[now_level].shape[1]*0.1:
-            deduplicate_results.append([[x, y], sim])
-        else:
-            if sim > deduplicate_results[dis_sort[0]][1]:
-                del deduplicate_results[dis_sort[0]]
-                deduplicate_results.append([[x, y], sim])
-
-#
-img_res = img.copy()
-w, h = template_gray.shape[::-1]
-
-for pt, similarity in deduplicate_results:
-    cv2.rectangle(img_res, (pt[1] - w//2, pt[0] - h//2), (pt[1] + w//2, pt[0] + h//2), (0, 0, 255), 2)
-for pt, similarity in deduplicate_results:
-    cv2.putText(img_res, f'center [{pt[1]},{pt[0]}]', (pt[1] - 50, pt[0] - 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1, cv2.LINE_AA)
-    cv2.putText(img_res, f'score {round(similarity, 3)}', (pt[1] - 50, pt[0] - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1, cv2.LINE_AA)
-
-# cv2.imshow('result', img_res)
-# cv2.waitKey(0)
-
-imgbaseName = imgName.split('.')[0]
-cv2.imwrite(f'./result/{imgbaseName}.jpg', img_res)
+    print(time.time() - st)
